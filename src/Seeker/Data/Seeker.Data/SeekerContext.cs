b962d3ca9fbbebeeca54611ba6da6b171c6e4223
DamnStation <radarawow@gmail.com>
2019-07-20 18:26:35 +0300

    Created new project added SeekerIdentity

---
+++ src/Seeker/Data/Seeker.Data/SeekerContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Seeker.Data.Models;
+
+namespace Seeker.Data
+{
+    public class SeekerContext : IdentityDbContext<SeekerUser>
+    {
+        public SeekerContext(DbContextOptions<SeekerContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            // Customize the ASP.NET Identity model and override the defaults if needed.
+            // For example, you can rename the ASP.NET Identity table names and more.
+            // Add your customizations after calling base.OnModelCreating(builder);
+        }
+    }
+}
---
+++ src/Seeker/Web/Seeker.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Seeker.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
